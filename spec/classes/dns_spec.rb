d1a49569f38d8bada13b8836b35a3e1787f7de66
b4ldr <github@johnbond.org>
2016-12-15 16:03:59 +0000

    update

--- spec/classes/dns_spec.rb
+++ spec/classes/dns_spec.rb
@@ -71,7 +71,7 @@ describe 'dns' do
         it { is_expected.to contain_class('dns::params') }
         it do
           is_expected.to contain_package('zonecheck').with(
-            'ensure' => '1.0.13',
+            'ensure' => '1.0.14',
             'provider' => 'pip'
           )
         end
