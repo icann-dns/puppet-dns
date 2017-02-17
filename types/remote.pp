4ee978312e43ec3645a0198195949184bd7d9395
b4ldr <github@johnbond.org>
2017-02-17 18:27:47 +0000

    remove remote type as its now a define

--- types/remote.pp
+++
@@ -1,6 +0,0 @@
-type Dns::Remote = Struct[{
-  address4  => Optional[Variant[Tea::Ipv4, Tea::Ipv4_cidr]],
-  address6  => Optional[Variant[Tea::Ipv6, Tea::Ipv6_cidr]],
-  tsig_name => Optional[String],
-  port      => Optional[Tea::Port],
-}]
