37429b3a561bc2dae702eb904c7dfbe7c93807ef
John Bond <github@johnbond.org>
2017-09-20 12:31:36 +0100

    update sync file

--- spec/acceptance/05_dns_virtualbox_master_notify_nsd_knot_spec.rb
+++ spec/acceptance/05_dns_virtualbox_master_notify_nsd_knot_spec.rb
@@ -5,16 +5,16 @@ require 'spec_helper_acceptance'
 if ENV['BEAKER_TESTMODE'] == 'agent' && ENV['VIRTUALBOX'] == 'yes'
   describe 'knot class' do
     context 'test notifies nsd -> knot' do
-      dnstop    = find_host_with_role('dnstop')
-      dnstop_ip = '10.255.1.3'
-      dnsedge     = find_host_with_role('dnsedge')
-      dnsedge_ip  = '10.255.1.4'
+      dnsmaster    = find_host_with_role('dnsmaster')
+      dnsmaster_ip = '10.255.1.3'
+      dnsslave     = find_host_with_role('dnsslave')
+      dnsslave_ip  = '10.255.1.4'
       example_zone = <<EOS
 example.com. 3600 IN SOA sns.dns.icann.org. noc.dns.icann.org. 1 7200 3600 1209600 3600
 example.com. 86400 IN NS a.iana-servers.net.
 example.com. 86400 IN NS b.iana-servers.net.
 EOS
-      dnstop_pp = <<-EOS
+      dnsmaster_pp = <<-EOS
       class {'::dns':
         daemon  => 'nsd',
         imports => ['nofiy_test'],
@@ -28,19 +28,19 @@ EOS
         },
       }
       EOS
-      dnsedge_pp = <<-EOS
+      dnsslave_pp = <<-EOS
       class {'::dns':
         daemon  => 'knot',
         exports => ['nofiy_test'],
-        default_tsig_name => '#{dnsedge}-test',
+        default_tsig_name => '#{dnsslave}-test',
         tsigs    => {
-          '#{dnsedge}-test' => {
+          '#{dnsslave}-test' => {
             'data' => 'qneKJvaiXqVrfrS4v+Oi/9GpLqrkhSGLTCZkf0dyKZ0='
           },
         },
         remotes => {
           'top_server' => {
-            'address4'  => '#{dnstop_ip}',
+            'address4'  => '#{dnsmaster_ip}',
           }
         },
         zones   => {
@@ -49,29 +49,29 @@ EOS
       }
       EOS
       it 'run puppet a bunch of times' do
-        execute_manifest_on(dnstop, dnstop_pp, catch_failures: true)
-        execute_manifest_on(dnsedge, dnsedge_pp, catch_failures: true)
-        execute_manifest_on(dnstop, dnstop_pp, catch_failures: true)
+        execute_manifest_on(dnsmaster, dnsmaster_pp, catch_failures: true)
+        execute_manifest_on(dnsslave, dnsslave_pp, catch_failures: true)
+        execute_manifest_on(dnsmaster, dnsmaster_pp, catch_failures: true)
       end
       it 'clean puppet run on dns top' do
-        expect(execute_manifest_on(dnstop, dnstop_pp, catch_failures: true).exit_code).to eq 0
+        expect(execute_manifest_on(dnsmaster, dnsmaster_pp, catch_failures: true).exit_code).to eq 0
       end
-      it 'clean puppet run on dns dnsedge' do
-        expect(execute_manifest_on(dnsedge, dnsedge_pp, catch_failures: true).exit_code).to eq 0
+      it 'clean puppet run on dns dnsslave' do
+        expect(execute_manifest_on(dnsslave, dnsslave_pp, catch_failures: true).exit_code).to eq 0
       end
-      describe service('nsd'), node: dnstop do
+      describe service('nsd'), node: dnsmaster do
         it { is_expected.to be_running }
       end
-      describe port(53), node: dnstop do
+      describe port(53), node: dnsmaster do
         it { is_expected.to be_listening }
       end
-      describe service('knot'), node: dnsedge do
+      describe service('knot'), node: dnsslave do
         it { is_expected.to be_running }
       end
-      describe port(53), node: dnsedge do
+      describe port(53), node: dnsslave do
         it { is_expected.to be_listening }
       end
-      describe command("dig +short soa example.com. @#{dnstop_ip}"), node: dnstop do
+      describe command("dig +short soa example.com. @#{dnsmaster_ip}"), node: dnsmaster do
         its(:exit_status) { is_expected.to eq 0 }
         its(:stdout) do
           is_expected.to match(
@@ -79,7 +79,7 @@ EOS
           )
         end
       end
-      describe command("dig +short soa example.com. @#{dnsedge_ip}"), node: dnsedge do
+      describe command("dig +short soa example.com. @#{dnsslave_ip}"), node: dnsslave do
         its(:exit_status) { is_expected.to eq 0 }
         its(:stdout) do
           is_expected.to match(
@@ -87,13 +87,13 @@ EOS
           )
         end
       end
-      describe command('sed -i \'s/1/2/\' /var/lib/nsd/zone/example.com'), node: dnstop do
+      describe command('sed -i \'s/1/2/\' /var/lib/nsd/zone/example.com'), node: dnsmaster do
         its(:exit_status) { is_expected.to eq 0 }
       end
-      describe command('service nsd restart'), node: dnstop do
+      describe command('service nsd restart'), node: dnsmaster do
         its(:exit_status) { is_expected.to eq 0 }
       end
-      describe command("dig +short soa example.com. @#{dnstop_ip}"), node: dnstop do
+      describe command("dig +short soa example.com. @#{dnsmaster_ip}"), node: dnsmaster do
         let(:pre_command) { 'sleep 5'  }
 
         its(:exit_status) { is_expected.to eq 0 }
@@ -103,7 +103,7 @@ EOS
           )
         end
       end
-      describe command("dig +short soa example.com. @#{dnsedge_ip}"), node: dnsedge do
+      describe command("dig +short soa example.com. @#{dnsslave_ip}"), node: dnsslave do
         its(:exit_status) { is_expected.to eq 0 }
         its(:stdout) do
           is_expected.to match(
